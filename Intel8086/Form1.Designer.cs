abbe66a593fb872fbc973bd0b407592b89dfe2ff
krystianpetek <krystianpetek2@gmail.com>
2021-03-27 17:10:02 +0100

    commit 2
    już trochę zrobione, działa combobox, clear, przypisz działa połowicznie

--- Intel8086/Form1.Designer.cs
+++ Intel8086/Form1.Designer.cs
@@ -33,56 +33,233 @@ namespace Intel8086
             this.bxLabel = new System.Windows.Forms.Label();
             this.cxLabel = new System.Windows.Forms.Label();
             this.dxLabel = new System.Windows.Forms.Label();
+            this.axView = new System.Windows.Forms.TextBox();
+            this.bxView = new System.Windows.Forms.TextBox();
+            this.cxView = new System.Windows.Forms.TextBox();
+            this.dxView = new System.Windows.Forms.TextBox();
+            this.dxText = new System.Windows.Forms.TextBox();
+            this.cxText = new System.Windows.Forms.TextBox();
+            this.bxText = new System.Windows.Forms.TextBox();
+            this.axText = new System.Windows.Forms.TextBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.comboBoxFROM = new System.Windows.Forms.ComboBox();
+            this.comboBoxTO = new System.Windows.Forms.ComboBox();
+            this.movLabel = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // axLabel
             // 
             this.axLabel.AutoSize = true;
+            this.axLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.axLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.axLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.axLabel.Location = new System.Drawing.Point(22, 13);
             this.axLabel.Name = "axLabel";
-            this.axLabel.Size = new System.Drawing.Size(22, 15);
+            this.axLabel.Size = new System.Drawing.Size(22, 18);
             this.axLabel.TabIndex = 0;
             this.axLabel.Text = "AX";
-            this.axLabel.Click += new System.EventHandler(this.label1_Click);
             // 
             // bxLabel
             // 
             this.bxLabel.AutoSize = true;
-            this.bxLabel.Location = new System.Drawing.Point(22, 32);
+            this.bxLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.bxLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bxLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.bxLabel.Location = new System.Drawing.Point(22, 39);
             this.bxLabel.Name = "bxLabel";
-            this.bxLabel.Size = new System.Drawing.Size(21, 15);
+            this.bxLabel.Size = new System.Drawing.Size(22, 18);
             this.bxLabel.TabIndex = 1;
             this.bxLabel.Text = "BX";
             // 
             // cxLabel
             // 
             this.cxLabel.AutoSize = true;
-            this.cxLabel.Location = new System.Drawing.Point(22, 51);
+            this.cxLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.cxLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cxLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.cxLabel.Location = new System.Drawing.Point(22, 65);
             this.cxLabel.Name = "cxLabel";
-            this.cxLabel.Size = new System.Drawing.Size(22, 15);
+            this.cxLabel.Size = new System.Drawing.Size(22, 18);
             this.cxLabel.TabIndex = 2;
             this.cxLabel.Text = "CX";
             // 
             // dxLabel
             // 
             this.dxLabel.AutoSize = true;
-            this.dxLabel.Location = new System.Drawing.Point(22, 70);
+            this.dxLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.dxLabel.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dxLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.dxLabel.Location = new System.Drawing.Point(22, 91);
             this.dxLabel.Name = "dxLabel";
-            this.dxLabel.Size = new System.Drawing.Size(22, 15);
+            this.dxLabel.Size = new System.Drawing.Size(22, 18);
             this.dxLabel.TabIndex = 3;
             this.dxLabel.Text = "DX";
+            // 
+            // axView
+            // 
+            this.axView.Enabled = false;
+            this.axView.Location = new System.Drawing.Point(50, 10);
+            this.axView.Name = "axView";
+            this.axView.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.axView.Size = new System.Drawing.Size(54, 23);
+            this.axView.TabIndex = 4;
+            this.axView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bxView
+            // 
+            this.bxView.Enabled = false;
+            this.bxView.Location = new System.Drawing.Point(50, 36);
+            this.bxView.Name = "bxView";
+            this.bxView.Size = new System.Drawing.Size(54, 23);
+            this.bxView.TabIndex = 5;
+            this.bxView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cxView
+            // 
+            this.cxView.Enabled = false;
+            this.cxView.Location = new System.Drawing.Point(50, 62);
+            this.cxView.Name = "cxView";
+            this.cxView.Size = new System.Drawing.Size(54, 23);
+            this.cxView.TabIndex = 6;
+            this.cxView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dxView
+            // 
+            this.dxView.Enabled = false;
+            this.dxView.Location = new System.Drawing.Point(50, 88);
+            this.dxView.Name = "dxView";
+            this.dxView.Size = new System.Drawing.Size(54, 23);
+            this.dxView.TabIndex = 7;
+            this.dxView.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dxText
+            // 
+            this.dxText.Location = new System.Drawing.Point(110, 88);
+            this.dxText.MaxLength = 4;
+            this.dxText.Name = "dxText";
+            this.dxText.Size = new System.Drawing.Size(54, 23);
+            this.dxText.TabIndex = 11;
+            this.dxText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cxText
+            // 
+            this.cxText.Location = new System.Drawing.Point(110, 62);
+            this.cxText.MaxLength = 4;
+            this.cxText.Name = "cxText";
+            this.cxText.Size = new System.Drawing.Size(54, 23);
+            this.cxText.TabIndex = 10;
+            this.cxText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bxText
+            // 
+            this.bxText.Location = new System.Drawing.Point(110, 36);
+            this.bxText.MaxLength = 4;
+            this.bxText.Name = "bxText";
+            this.bxText.Size = new System.Drawing.Size(54, 23);
+            this.bxText.TabIndex = 9;
+            this.bxText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // axText
+            // 
+            this.axText.Location = new System.Drawing.Point(110, 10);
+            this.axText.MaxLength = 4;
+            this.axText.Name = "axText";
+            this.axText.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.axText.Size = new System.Drawing.Size(54, 23);
+            this.axText.TabIndex = 8;
+            this.axText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonOk.Location = new System.Drawing.Point(189, 13);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(114, 23);
+            this.buttonOk.TabIndex = 12;
+            this.buttonOk.Text = "PRZYPISZ";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // comboBoxFROM
+            // 
+            this.comboBoxFROM.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.comboBoxFROM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFROM.FormattingEnabled = true;
+            this.comboBoxFROM.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.comboBoxFROM.Location = new System.Drawing.Point(50, 130);
+            this.comboBoxFROM.Name = "comboBoxFROM";
+            this.comboBoxFROM.Size = new System.Drawing.Size(54, 23);
+            this.comboBoxFROM.TabIndex = 13;
+            this.comboBoxFROM.SelectedIndexChanged += new System.EventHandler(this.comboBoxFROM_SelectedIndexChanged);
+            // 
+            // comboBoxTO
+            // 
+            this.comboBoxTO.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTO.FormattingEnabled = true;
+            this.comboBoxTO.Items.AddRange(new object[] {
+            "AX",
+            "BX",
+            "CX",
+            "DX"});
+            this.comboBoxTO.Location = new System.Drawing.Point(110, 130);
+            this.comboBoxTO.Name = "comboBoxTO";
+            this.comboBoxTO.Size = new System.Drawing.Size(54, 23);
+            this.comboBoxTO.TabIndex = 14;
+            this.comboBoxTO.SelectedIndexChanged += new System.EventHandler(this.comboBoxTO_SelectedIndexChanged);
+            // 
+            // movLabel
+            // 
+            this.movLabel.AutoSize = true;
+            this.movLabel.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.movLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.movLabel.Location = new System.Drawing.Point(50, 167);
+            this.movLabel.Name = "movLabel";
+            this.movLabel.Size = new System.Drawing.Size(0, 20);
+            this.movLabel.TabIndex = 15;
+            this.movLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonClear.Location = new System.Drawing.Point(189, 39);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(114, 23);
+            this.buttonClear.TabIndex = 16;
+            this.buttonClear.Text = "WYCZYŚĆ";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.movLabel);
+            this.Controls.Add(this.comboBoxTO);
+            this.Controls.Add(this.comboBoxFROM);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.dxText);
+            this.Controls.Add(this.cxText);
+            this.Controls.Add(this.bxText);
+            this.Controls.Add(this.axText);
+            this.Controls.Add(this.dxView);
+            this.Controls.Add(this.cxView);
+            this.Controls.Add(this.bxView);
+            this.Controls.Add(this.axView);
             this.Controls.Add(this.dxLabel);
             this.Controls.Add(this.cxLabel);
             this.Controls.Add(this.bxLabel);
             this.Controls.Add(this.axLabel);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,12 +268,22 @@ namespace Intel8086
         #endregion
 
         private System.Windows.Forms.Label axLabel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label bxLabel;
         private System.Windows.Forms.Label cxLabel;
         private System.Windows.Forms.Label dxLabel;
+        private System.Windows.Forms.TextBox axView;
+        private System.Windows.Forms.TextBox bxView;
+        private System.Windows.Forms.TextBox cxView;
+        private System.Windows.Forms.TextBox dxView;
+        private System.Windows.Forms.TextBox dxText;
+        private System.Windows.Forms.TextBox axText;
+        private System.Windows.Forms.TextBox bxText;
+        private System.Windows.Forms.TextBox cxText;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.ComboBox comboBoxFROM;
+        private System.Windows.Forms.ComboBox comboBoxTO;
+        private System.Windows.Forms.Label movLabel;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
 
--- Intel8086/Form1.cs
+++ Intel8086/Form1.cs
@@ -12,14 +12,118 @@ namespace Intel8086
 {
     public partial class Form1 : Form
     {
+        string ax;
+        string bx;
+        string cx;
+        string dx;
+
+        string pierwszy;
+        string drugi;
+
         public Form1()
         {
             InitializeComponent();
+            ax = "0000";
+            bx = "0000";
+            cx = "0000";
+            dx = "0000";
+            axView.Text = ax;
+            axText.Text = ax;
+            bxView.Text = bx;
+            bxText.Text = bx;
+            cxView.Text = cx;
+            cxText.Text = cx;
+            dxView.Text = dx;
+            dxText.Text = dx;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            if (axText.Text.Length == 4)
+                axView.Text = axText.Text.ToUpper();
+
+            if (bxText.Text.Length == 4)
+                bxView.Text = bxText.Text.ToUpper();
+
+            if (cxText.Text.Length == 4)
+                cxView.Text = cxText.Text.ToUpper();
+
+            if (dxText.Text.Length == 4)
+                dxView.Text = dxText.Text.ToUpper();
+
+            if ((comboBoxFROM.Text.Length > 0) && (comboBoxTO.Text.Length > 0))
+                movLabel.Text = $"MOV {pierwszy}, {drugi}";
+
+            else if (comboBoxFROM.Text.Length > 0)
+                movLabel.Text = $"MOV {pierwszy}";
+            else
+                movLabel.Text = "";
+        }
+
+        private void comboBoxFROM_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+            switch(comboBoxFROM.Text)
+            {
+                case "AX":
+                    pierwszy = "AX";
+                    break;
+                case "BX":
+                    pierwszy = "BX";
+                    break;
+                case "CX":
+                    pierwszy = "CX";
+                    break;
+                case "DX":
+                    pierwszy = "DX";
+                    break;
+            }
+
+            /*
+            if(comboBoxFROM.Text == "AX") movLabel.Text = "MOV AX";
+            
+            if(comboBoxFROM.Text == "BX") movLabel.Text = "MOV BX";
+
+            if (comboBoxFROM.Text == "CX") movLabel.Text = "MOV CX";
+
+            if (comboBoxFROM.Text == "DX") movLabel.Text = "MOV DX";
+            */
+        }
+
+        private void comboBoxTO_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxTO.Text)
+            {
+                case "AX":
+                    drugi = "AX";
+                    break;
+                case "BX":
+                    drugi = "BX";
+                    break;
+                case "CX":
+                    drugi = "CX";
+                    break;
+                case "DX":
+                    drugi = "DX";
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            comboBoxFROM.SelectedIndex = -1;
+            comboBoxTO.SelectedIndex = -1;
+            movLabel.Text = "";
+            axText.Text = "";
+            bxText.Text = "";
+            cxText.Text = "";
+            dxText.Text = "";
         }
     }
 }
